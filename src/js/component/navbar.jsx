bab574ca9fd7bfa98e4ac4f3a77768c1b9cd2c99
JMP160880 <javiermoranpando@gmail.com>
2023-01-07 19:31:27 +0000

    7-1-23-fin jornada

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -4,6 +4,7 @@ const MyNavbar= () => {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-dark">
   <div className="container-fluid">
+  <div className="col-sm-2 display-none"></div> 
     <a className="navbar-brand text-light" href="#">Start Bootstrap</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -11,6 +12,7 @@ const MyNavbar= () => {
     <div className="collapse navbar-collapse" id="navbarText">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
       </ul>
+      <div className="col-sm-4 display-none"></div> 
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
